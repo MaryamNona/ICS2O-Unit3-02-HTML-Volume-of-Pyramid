14c7057540ea16b95bee79c234e30a12b3ad3d65
MaryamNona <>
2021-05-24 03:27:05 +0000

    added index.html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ICS2O-Unit3-02-HTML-Volume-of-Pyramid
+
+[![GitHub's Super Linter](https://github.com/MaryamNona/ICS2O-Unit3-02-HTML-Volume-of-Pyramid/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MaryamNona/ICS2O-Unit3-02-HTML-Volume-of-Pyramid/actions)
+
+This website can be found at: [https://MaryamNona.github.io/ICS2O-Unit3-02-HTML-Volume-of-Pyramid](https://MaryamNona.github.io/ICS2O-Unit3-02-HTML-Volume-of-Pyramid)
---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Maryam Nona
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function addAgeClicked () {
+  // this function does area 
+
+  // input
+  const length = parseFloat(document.getElementById("length-of-pyramid").value)
+  const width = parseFloat(document.getElementById("width-of-pyramid").value)
+  const height = parseFloat(document.getElementById("height-of-pyramid").value)
+
+  // process
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + " mm³"
+}
